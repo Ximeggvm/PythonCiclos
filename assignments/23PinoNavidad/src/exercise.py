d0b2690fdbd7dfa8b58c4059e7d8a1a18e66dbd9
Ximeggvm <110927159+Ximeggvm@users.noreply.github.com>
2022-09-07 23:09:06 -0500

    Update exercise.py

--- assignments/23PinoNavidad/src/exercise.py
+++ assignments/23PinoNavidad/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
-    #escribe tu código abajo de esta línea
+    #escribe tu código abajo de esta línea.
+def p_a():
+    altura=int(input("Dame la altura de la piramide: "))
+    ast=1
+    for espacios in range (altura,0,-1):
+        print((espacios-1)*" ",ast*"*",end="\n")
+        ast= ast+2
+    return (" ")
 
+print(p_a())
 
 if __name__=='__main__':
     main()
